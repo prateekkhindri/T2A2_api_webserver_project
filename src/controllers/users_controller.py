e952139f79c152834a76ecc4b8ddf8542e2fc5cb
PrateekKhindri <prateek.khindri90@gmail.com>
2023-03-16 23:38:02 +1100

    T2A2-002 - Added endpoints to promote a users account to admin, get all users, get all buyers, get all sellers and get all admin users

--- src/controllers/users_controller.py
+++ src/controllers/users_controller.py
@@ -104,3 +104,97 @@ class UserView(Resource):
             'user': updated_user
         }
         return success_response, 200
+
+
+# Endpoint to promote a user to admin - ONLY ADMIN CAN ACTION
+@api.route("promote-admin/")
+class PromoteAdminView(Resource):
+    @api.expect(promote_admin)
+    @token_required
+    @permission_required
+    def post(self):
+        request_data = request.get_json()
+        user = User.find_by_id(request_data.get("id"))
+        if not user:
+            error_response['message'] = "User does not exist"
+            return error_response, 400
+        user.update({"is_admin": True})
+
+        user_schema = UserSchema(exclude=admin_exclude)
+        updated_user = user_schema.dump(user)
+        success_response['message'] = f'User {updated_user["first_name"] + " " + updated_user["last_name"]} has been promoted to admin'
+        success_response['data'] = {
+            'user': updated_user
+        }
+        return success_response, 200
+
+
+# Endpoint to get all users from the database - ONLY ADMIN CAN ACTION
+@api.route('all-users/')
+class AllUserView(Resource):
+    @token_required
+    @permission_required
+    def get(self):
+        # The query below is used to retrieve all users data from the database
+        user = User.query.all()
+
+        if not user:
+            error_response['message'] = "No users found"
+            return error_response, 404
+
+        user_schema = UserSchema(exclude=admin_exclude, many=True)
+        return user_schema.dump(user)
+
+
+# Endpoint to get all buyers - ONLY ADMIN CAN ACTION
+@api.route('all-users/buyer/')
+class AllUserBuyerView(Resource):
+    @token_required
+    @permission_required
+    def get(self):
+
+        # The query below is used to retrieve all non-seller and non-admin users from the database
+        user = User.query.filter_by(is_seller=False, is_admin=False)
+
+        if not user:
+            error_response['message'] = "No users found"
+            return error_response, 404
+
+        user_schema = UserSchema(exclude=admin_exclude, many=True)
+        return user_schema.dump(user)
+
+
+# Endpoint to get all sellers - ONLY ADMIN CAN ACTION
+@api.route('all-users/seller/')
+class AllUserSellerView(Resource):
+    @token_required
+    @permission_required
+    def get(self):
+
+        # The query below is used to retrieve all users that are sellers from the database
+        seller = User.query.filter_by(is_seller=True)
+
+        if not seller:
+            error_response['message'] = "No sellers found"
+            return error_response, 404
+
+        user_schema = UserSchema(exclude=admin_exclude, many=True)
+        return user_schema.dump(seller)
+
+
+# Endpoint to get all admins - ONLY ADMIN CAN ACTION
+@api.route('all-users/admin/')
+class AllUserAdminView(Resource):
+    @token_required
+    @permission_required
+    def get(self):
+
+        # The query below is used to retrieve all admin users from the database
+        admin = User.query.filter_by(is_admin=True)
+
+        if not admin:
+            error_response['message'] = "No admin users found"
+            return error_response, 404
+
+        user_schema = UserSchema(exclude=admin_exclude, many=True)
+        return user_schema.dump(admin)
