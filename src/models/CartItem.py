d5b7602c9134b6da0860264b4cef3a6ba1859dae
PrateekKhindri <prateek.khindri90@gmail.com>
2023-03-17 15:46:21 +1100

    T2A2-007 - Added CartItem model

---
+++ src/models/CartItem.py
@@ -0,0 +1,21 @@
+from src.models.BaseModel import BaseModel
+from src.extension import db
+
+
+# CartItem Model class
+class CartItem(BaseModel):
+
+    __tablename__ = 'cart_items'
+    user_id = db.Column(db.Integer, db.ForeignKey(
+        "users.id", ondelete='CASCADE'), nullable=False)
+    seller_id = db.Column(
+        db.Integer, db.ForeignKey("users.id", ondelete='CASCADE'), nullable=False)
+    cart_id = db.Column(db.Integer, db.ForeignKey(
+        'carts.id', ondelete='CASCADE'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey(
+        'products.id', ondelete='CASCADE'), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False, default=1)
+    user = db.relationship(
+        'User', backref='cart_items', lazy='joined', foreign_keys=[user_id])
+    product = db.relationship(
+        'Product', backref=db.backref('cart_items', cascade='all, delete-orphan'), lazy='joined', foreign_keys=[product_id])
