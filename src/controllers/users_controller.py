7a2baa8410aee770e53d050231655d8a3e22d80e
PrateekKhindri <prateek.khindri90@gmail.com>
2023-03-17 12:11:47 +1100

    T2A2-002 - Added endpoints to get all products listed by a seller, update a product, delete a product and create a product

--- src/controllers/users_controller.py
+++ src/controllers/users_controller.py
@@ -6,10 +6,14 @@ from src.utils.responses import success_response, error_response
 from src.utils.token_required import token_required
 from flask import request
 from src.extension import session
+from src.validators.product import ProductValidators
+from src.schema.response.Product import ProductSchema
+from src.models.Product import Product
 from src.schema.response.User import UserSchema
 from src.validators.user import UserValidators
 from src.schema.request.User import user_fields, delete_fields
 from src.models.User import User
+from src.schema.request.Product import product_var, product_delete as product_del_variable, product_update
 
 
 api = Namespace('user', description='User Related Operations')
@@ -32,6 +36,9 @@ search_parser.add_argument('name', required=False,
 user_model = api.model("user_model", user_fields)
 delete_user = api.model("delete_user", delete_fields)
 promote_admin = api.model("promote_admin", delete_fields)
+product_model = api.model('Product', product_var)
+product_delete = api.model('Product-Delete', product_del_variable)
+product_update_model = api.model('Product-Update-Model', product_update)
 user_exclude = ['created_at', 'updated_at', "password", "is_admin"]
 admin_exclude = ['created_at', 'updated_at', "password"]
 
@@ -221,4 +228,132 @@ class UserSearch(Resource):
 
         success_response['message'] = 'User successfully fetched'
         success_response['data'] = user_schema.dump(user)
+        return success_response, 200
+
+
+# Endpoint to get all products listed by a seller
+@api.route('<string:username>/products/')
+class SellerListedProducts(Resource):
+    @token_required
+    @permission_seller
+    def get(self, username):
+        user_id = session.current_user.get('id')
+
+        # The query below is used to retrieve all products that belong to a specific seller, as identified by their seller_id
+        product = Product.query.filter_by(seller_id=user_id)
+
+        if not session.current_user.get('username') == username:
+            error_response['message'] = 'The username provided is invalid'
+            return error_response, 404
+
+        if not product:
+            error_response['message'] = 'No Product Found'
+            return error_response, 404
+
+        product_schema = ProductSchema(
+            exclude=EXCLUDED_FIELDS, many=True)
+        product_data = product_schema.dump(product)
+        success_response['message'] = 'Products Successfully fetched'
+        success_response['data'] = product_data
+        return success_response, 200
+
+
+# Endpoint to create a product by a seller
+@api.route('<string:username>/product/')
+class SellerListedSingleProduct(Resource):
+
+    @api.expect(product_model)
+    @token_required
+    @permission_seller
+    def post(self, username):
+        user_id = session.current_user.get("id")
+
+        if not session.current_user.get('username') == username:
+            error_response['message'] = 'Incorrect username provided'
+            return error_response, 401
+
+        request_data = request.get_json()
+        ProductValidators.validate(request_data)
+
+        request_data = remove_space(request_data)
+        request_data['seller_id'] = user_id
+        request_data['name'] = request_data['name'].lower()
+        new_product = Product(**request_data)
+        new_product.save()
+
+        product_schema = ProductSchema(exclude=EXCLUDED_FIELDS)
+        product_data = product_schema.dump(new_product)
+
+        success_response['message'] = 'Product successfully created'
+        success_response['data'] = {
+            'product': product_data
+        }
+        return success_response, 201
+
+    # Endpoint to delete a product by a seller
+    @api.expect(product_delete)
+    @token_required
+    @permission_seller
+    def delete(self, username):
+        request_data = request.get_json()
+        user_id = session.current_user.get('id')
+
+        if not session.current_user.get('username') == username:
+            error_response['message'] = 'Incorrect username provided'
+            return error_response, 401
+
+        product = Product.find_by_id(request_data.get("id"))
+
+        if not product:
+            error_response['message'] = 'This product does not exist'
+            return error_response, 400
+
+        if product.seller_id != user_id:
+            error_response['message'] = 'You are not allowed to delete this product'
+            return error_response, 404
+        product.delete()
+
+        product_schema = ProductSchema(exclude=EXCLUDED_FIELDS)
+        deleted_product = product_schema.dump(product)
+        success_response['message'] = f'Product {deleted_product["name"]} has been deleted successfully'
+        success_response['data'] = {
+            'product': deleted_product
+        }
+        return success_response, 200
+
+    # Endpoint to update a product by a seller
+    @api.expect(product_update_model)
+    @token_required
+    @permission_seller
+    def patch(self, username):
+        user = session.current_user
+
+        request_data = request.get_json()
+        id = request_data['id']
+        del request_data['id']
+        ProductValidators.validate(request_data)
+        request_data = remove_space(request_data)
+        request_data['name'] = request_data['name'].lower()
+        updated_product = Product.find_by_id(id)
+
+        if not session.current_user.get('username') == username:
+            error_response['message'] = 'Incorrect username provided'
+            return error_response, 401
+
+        if not updated_product:
+            error_response['message'] = 'This product does not exist'
+            return error_response, 400
+
+        if user.get('id') != updated_product.seller_id:
+            error_response['message'] = 'You are not allowed to update this product'
+            return error_response, 404
+
+        updated_product.update(request_data)
+        product_schema = ProductSchema(exclude=EXCLUDED_FIELDS)
+        product_data = product_schema.dump(updated_product)
+
+        success_response['message'] = f'Product {product_data["name"]} has been updated successfully'
+        success_response['data'] = {
+            'product': product_data
+        }
         return success_response, 200
