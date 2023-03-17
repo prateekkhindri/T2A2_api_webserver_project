ef68c5b0eeeb32ae3f8fcbb3497ed5c29070d157
PrateekKhindri <prateek.khindri90@gmail.com>
2023-03-17 15:11:45 +1100

    T2A2-008 - Added AddressValidators class

---
+++ src/validators/address.py
@@ -0,0 +1,26 @@
+from src.validators import raise_validation_error
+import re
+
+
+# Address validators class
+class AddressValidators:
+
+    # Validates the address data
+    @classmethod
+    def validate(cls, data: dict, is_update=False):
+        required_fields = ['street_address', 'suburb', 'state', 'postcode']
+        if is_update:
+            required_fields.remove('default')
+
+        regex = {
+            'street_address': r'^\d{1,5}\s[a-zA-Z0-9\s\.,-]+$',
+            'suburb': r'^[a-zA-Z\s]{1,40}$',
+            'state': r'^(VIC|NSW|QLD|ACT|SA|WA|NT|TAS)$',
+            'postcode': r'^\d{4}$'
+        }
+
+        for field in required_fields:
+            if not data.get(field) or not data.get(field).strip():
+                raise_validation_error(f'The {field} is required')
+            if not re.match(regex[field], data.get(field).strip()):
+                raise_validation_error(f'The {field} is not valid')
